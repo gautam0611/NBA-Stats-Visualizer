fb0dc4d26f99bccab942a766bbaf29938b682c03
Gautam Narayan <gau.narayan@gmail.com>
2024-07-27 18:34:38 -0400

    started scraper

--- backend/app/scrape_stats.py
+++ backend/app/scrape_stats.py
@@ -1,21 +1,25 @@
-class WebScrapeNBAStatistics:
-    def scrape_players():
-        pass
+from enum import Enum
+import requests
+from bs4 import BeautifulSoup
 
-    def scrape_seasons():
-        pass
+class StatsType(Enum):
+    TEAM = "team"
+    PLAYER = "player"
+    ROSTER = "stats"
+    GAMES = "games"
+    RECORD = "record"
+    SEASON = "season"
 
-    def scrape_teams():
-        pass
+def scrape_nba_statistics(stats_type: StatsType):
+    url = f"https://sports.yahoo.com/nba/teams/boston/{stats_type.value}/"
+    print(url)
+    r = requests.get(url)
 
-    def scrape_rosters():
-        pass
+    soup = BeautifulSoup(r.content, 'lxml')
 
-    def scrape_games():
-        pass
+    table = soup.find('div', attrs={'class': 'Ovx(s)'})
+    print(table)
 
-    def scrape_record():
-        pass
 
-    
-
+# playerData = scrape_nba_statistics(stats_type=StatsType.PLAYER)
+print(scrape_nba_statistics(stats_type=StatsType.ROSTER))
