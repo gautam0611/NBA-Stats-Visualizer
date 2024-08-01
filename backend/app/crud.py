ca3b467b6d88e380026d1150fc9055fe7374c0f5
Gautam Narayan <gau.narayan@gmail.com>
2024-07-31 20:39:23 -0400

    fixed all the get crud functions

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -36,57 +36,53 @@ def get_division(db: Session, division_id: int):
 
 
 # GET /teams/{conference_id}
-# get all of the teams in the specified conference
-def get_all_teams(db: Session, conference_id: int):
-    return (
-        db.query(models.Team).filter(models.Team.conference_id == conference_id).all()
-    )
+# get all of the teams in the specified division
+def get_all_teams(db: Session, division_id: int):
+    return db.query(models.Team).filter(models.Team.division_id == division_id).all()
 
 
 # GET /{team_id}?{conference_id}
-# get a specific team from a specific conference
-def get_team(db: Session, team_id: int, conference_id: int):
+# get a specific team from a specific division
+def get_team(db: Session, team_id: int, division_id: int):
     return (
         db.query(models.Team)
-        .filter(
-            and_(models.Team.id == team_id, models.Team.conference_id == conference_id)
-        )
+        .filter(and_(models.Team.id == team_id, models.Team.division_id == division_id))
         .first()
     )
 
 
-# GET /record/{record_int}?{season_id}?{team_id}
+# GET /record/{season_id}?{team_id}
 # gets the record for the specified season
-def get_record(db: Session, record_id: int, season_id: int, team_id: int):
+def get_record(db: Session, season_id: int, team_id: int):
     return (
         db.query(models.Record)
         .join(models.Season, models.Record.season_id == models.Season.id)
+        .join(models.Team, models.Record.team_id == models.Team.id)
         .filter(
             and_(
-                models.Record.id == record_id,
-                models.Season.id == season_id,
-                models.Season.team_id == team_id,
+                models.Record.season_id == season_id,
+                models.Record.team_id == team_id,
             )
         )
         .first()
     )
 
 
-# GET /roster/{roster_id}?{season_id}?{team_id}
-# get the roster for that specified season
-def get_roster(db: Session, roster_id: int, season_id: int, team_id: int):
-    return (
-        db.query(models.Roster)
-        .join(models.Season, models.Roster.season_id == models.Season.id)
-        .filter(
-            and_(
-                models.Roster.id == roster_id,
-                models.Season.id == season_id,
-                models.Season.team_id == team_id,
-            )
-        )
-        .first()
-    )
+# # GET /roster/{roster_id}?{season_id}?{team_id}
+# # get the roster for that specified season
+# def get_roster(db: Session, roster_id: int, season_id: int, team_id: int):
+#     return (
+#         db.query(models.Roster)
+#         .join(models.Season, models.Roster.season_id == models.Season.id)
+#         .filter(
+#             and_(
+#                 models.Roster.id == roster_id,
+#                 models.Season.id == season_id,
+#                 models.Season.team_id == team_id,
+#             )
+#         )
+#         .first()
+#     )
 
 
 # GET /games/{season_id}?{team_id}
@@ -95,7 +91,13 @@ def get_games(db: Session, season_id: int, team_id: int):
     return (
         db.query(models.Games)
         .join(models.Season, models.Games.season_id == models.Season.id)
-        .filter(and_(models.Season.id == season_id, models.Season.team_id == team_id))
+        .join(models.Team, models.Games.team_id == models.Team.id)
+        .filter(
+            and_(
+                models.Games.season_id == season_id,
+                models.Games.team_id == team_id,
+            )
+        )
         .all()
     )
 
@@ -105,12 +107,14 @@ def get_games(db: Session, season_id: int, team_id: int):
 def get_players(db: Session, team_id: int, season_id: int):
     return (
         db.query(models.Player)
-        .join(models.Roster, models.Player.roster_id == models.Roster.id)
-        .join(models.Season, models.Roster.season_id == models.Season.id)
+        .join(models.Season, models.Player.season_id == models.Season.id)
         .join(models.Team, models.Season.team_id == models.Team.id)
         .filter(and_(models.Team.id == team_id, models.Season.id == season_id))
         .all()
     )
+
+
+# --------------------------------------------------------------------------------------
 
 
 # POST /conference
@@ -160,6 +164,22 @@ def create_record(db: Session, record: RecordCreate):
     return db_record
 
 
+# POST /player/{team_id}?{season_id}
+def create_player(db: Session, player: PlayerCreate):
+    db_player = models.Player(
+        name=player.name,
+        points=player.points,
+        rebounds=player.rebounds,
+        assists=player.assists,
+        season_id=player.season_id,
+        team_id=player.team_id,
+    )
+    db.add(db_player)
+    db.commit()
+    db.refresh(db_player)
+    return db_player
+
+
 """
 @FIXME need to update this so that we can add foreign keys
 """
@@ -172,20 +192,6 @@ def create_record(db: Session, record: RecordCreate):
 #     db.commit()
 #     db.refresh(db_game)
 #     return db_game
-
-
-# # POST /player/{team_id}?{season_id}
-# def create_player(db: Session, player: PlayerCreate):
-#     db_player = models.Player(
-#         name=player.name,
-#         points=player.points,
-#         rebounds=player.rebounds,
-#         assists=player.assists,
-#     )
-#     db.add(db_player)
-#     db.commit()
-#     db.refresh(db_player)
-#     return db_player
 
 
 # # POST /roster/{season_id}?{team_id}
