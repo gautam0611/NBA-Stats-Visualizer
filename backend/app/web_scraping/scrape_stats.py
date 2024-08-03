acc82c82f0988c81a28ba5e0fb35c6f98ac71053
Gautam Narayan <gau.narayan@gmail.com>
2024-08-03 16:48:52 -0400

    adding seasons and years

--- backend/app/web_scraping/scrape_stats.py
+++ backend/app/web_scraping/scrape_stats.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from enum import Enum
 from typing import Dict, List
 import requests
@@ -14,11 +15,71 @@ from backend.app.web_scraping.stats_type import StatsType
 
 
 class ScrapeNBAStats:
+    def __init__(self, team_name, season) -> None:
+        self.team_name = team_name
+        self.season = season
+
+    def team_dict(self) -> dict[str, str]:
+        team_lower = self.team_name.lower().strip()
+
+        # ensure that user did not enter "Miami Heat"
+        if len(team_lower.split("")) > 1:
+            raise NameError("Not a valid team entry. Must be in the format 'celtics' ")
+
+        team_dict = {
+            "celtics": "boston",
+            "heat": "miami",
+            "nets": "brooklyn",
+            "knicks": "new-york",
+            "76ers": "philadelphia",
+            "raptors": "toronto",
+            "bulls": "chicago",
+            "cavaliers": "cleveland",  # could also be "cavs"
+            "pistons": "detroit",
+            "pistons": "indiana",
+            "bucks": "milwaukee",
+            "hawks": "atlanta",
+            "hornets": "charlotte",
+            "magic": "orlando",
+            "wizards": "washington",
+            "warriors": "golden-state",
+            "clippers": "la-clippers",
+            "lakers": "la-lakers",
+            "suns": "phoenix",
+            "kings": "sacramento",
+            "mavericks": "dallas",  # could also be "mavs"
+            "rockets": "houston",
+            "grizzlies": "memphis",
+            "pelicans": "new-orleans",
+            "spurs": "san-antonio",
+            "nuggets": "denver",
+            "wolves": "minnesota",  # could also be "timberwolves"
+            "thunder": "oklahoma-city",
+            "blazers": "portland",
+            "jazz": "utah",
+        }
+        return team_dict
+
+    def season_dict(self):
+        season_dict = {
+            "2023-2024": "2024",
+            "2022-2023": "2023",
+            "2021-2022": "2022",
+            "2020-2021": "2021",
+            "2019-2020": "2020",
+            "2018-2019": "2019",
+            "2017-2018": "2018",
+            "2016-2017": "2017",
+            "2015-2016": "2016",
+            "2014-2015": "2015",
+            "2013-2014": "2014",
+        }
+
     def scrape_nba_statistics(self):
         raise NotImplementedError("subclasses will handle implementation")
 
-    def json_body_to_return(self):
-        raise NotImplementedError("subclasses will handle implementation")
+    # def json_body_to_return(self):
+    #     raise NotImplementedError("subclasses will handle implementation")
 
 
 class ScrapeConference(ScrapeNBAStats):
@@ -52,9 +113,6 @@ class ScrapeDivisions(ScrapeNBAStats):
                 list_of_divisions.append(divisionName)
         return list_of_divisions
 
-    # def json_body_to_return(self):
-    #     return super().json_body_to_return()
-
 
 class ScrapeTeams(ScrapeNBAStats):
     # web scrapes all of the teams in each division
@@ -78,9 +136,6 @@ class ScrapeTeams(ScrapeNBAStats):
 
         return division_dict
 
-    # def json_body_to_return(self):
-    #     return super().json_body_to_return()
-
 
 class ScrapeSeasons(ScrapeNBAStats):
     # web scrapes all of the seasons in the past 10 years
@@ -97,39 +152,54 @@ class ScrapeSeasons(ScrapeNBAStats):
 
         return list_of_seasons
 
-    # def json_body_to_return(self):
-    #     return super().json_body_to_return()
-
 
 class ScrapePlayers(ScrapeNBAStats):
+    def __init__(self, team_name, season) -> None:
+        super().__init__(team_name, season)
+
     # web scrapes all of the players and their stats on each team
-    def scrape_nba_statistics(self):
-        list_of_nba_teams = ScrapeTeams().scrape_nba_statistics()
-        for team in list_of_nba_teams:
-            url = f"https://sports.yahoo.com/nba/teams/{team}/{StatsType.ROSTER.value}"
+    def scrape_nba_statistics(self) -> dict[str, dict[str, Decimal]]:
+        # get a list of all the players on specified team in a season
+        for player in self.team_name:
+            player_dict: dict[str, dict[str, Decimal]]
+            url = f"https://sports.yahoo.com/nba/teams/{self.team_name}/{StatsType.ROSTER.value}?season={self.season}"
             r = requests.get(url)
             soup = BeautifulSoup(r.content, "lxml")
 
-    # def json_body_to_return(self):
-    #     return super().json_body_to_return()
+            for tr in soup.find_all(
+                "tr",
+                class_=["Bgc(table-hover):h", "Bgc(table-hover):h Bgc(fuji-secondary)"],
+            ):
+                stats_dict: dict[str, Decimal] = {}
+                points = Decimal(tr.td[13].get_text().strip())
+                assists = Decimal(tr.td[17].get_text().strip())
+                rebounds = Decimal(tr.td[16].get_text().strip())
+
+                # add our stats to a dictionary for this player
+                stats_dict["points"] = points
+                stats_dict["assists"] = assists
+                stats_dict["rebounds"] = rebounds
+            # add the player and their stats to our dictionary
+            player_dict[player] = stats_dict
+        return player_dict
 
 
 class ScrapeRecord(ScrapeNBAStats):
+    def __init__(self, team_name, season) -> None:
+        super().__init__(team_name, season)
+
     # web scrapes the record of the specifed team in a specifed season
     def scrape_nba_statistics(self):
         pass
 
-    # def json_body_to_return(self):
-    #     return super().json_body_to_return()
-
 
 class ScrapeGames(ScrapeNBAStats):
+    def __init__(self, team_name, season) -> None:
+        super().__init__(team_name, season)
+
     # web scrapes the games of the specified team in a specified season
     def scrape_nba_statistics(self):
         pass
-
-    # def json_body_to_return(self):
-    #     return super().json_body_to_return()
 
 
 statistics = {
